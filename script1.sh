438b810f3c67c6d1b23c30822969d59ecbf0d2f7
nurvaibd24 <84995015+nurvaibd24@users.noreply.github.com>
2022-07-05 02:28:46 +0600

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cp /usr/share/zoneinfo/Asia/Riyadh /etc/localtime
 #Database Details
-HOST='172.105.';
-USER='loop';
-PASS='@@AlaMinb';
-DBNAME='loop';
+HOST='50.116.18.242';
+USER='loveyoun_rsshatipro';
+PASS='p{2@X(wng6}1';
+DBNAME='loveyoun_rsshatipro';
 
 install_require()
 {
